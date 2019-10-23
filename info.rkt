94d492f057e1fa712ceab1823afca31ffc80f04d
Eric Griffis <dedbox@gmail.com>
2019-10-23 10:11:05 -0700

    add GLSL to pkg deps

--- info.rkt
+++ info.rkt
@@ -1,6 +1,6 @@
 #lang info
 
 (define collection "graphics-engine")
-(define deps '("base" "opengl" "glm" "gui-lib" "reprovide-lang-lib"))
+(define deps '("base" "opengl" "glm" "glsl" "gui-lib" "reprovide-lang-lib"))
 (define build-deps '("racket-doc" "scribble-lib"))
 (define scribblings '(("scribblings/graphics-engine.scrbl")))
